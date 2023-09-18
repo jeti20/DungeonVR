ab04b90f39ed4b426bed1b511db93a983fa9a8d2
Dehydrogenase <michalrodak98@gmail.com>
2023-09-18 20:08:09 +0200

    media + death room imprv + hammers scripts

--- Horror VR/Assets/Anim/DeathRoom/DeathRoom.cs
+++ Horror VR/Assets/Anim/DeathRoom/DeathRoom.cs
@@ -8,6 +8,8 @@ public class DeathRoom : MonoBehaviour
     public Animator[] animators; // Tablica animacji, do której chcesz dodaæ obiekty
     private bool animacjeWlaczane = false;
 
+    public Animator animatorEndedKillPlayer;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !animacjeWlaczane)
@@ -19,7 +21,18 @@ public class DeathRoom : MonoBehaviour
                 Debug.Log("W³¹cz animacje");
             }
 
+            StartCoroutine(WaitForAnimationAndKillPlayer());
             animacjeWlaczane = true; // Zapobieganie wielokrotnemu uruchamianiu
+
         }
+    }
+
+    private IEnumerator WaitForAnimationAndKillPlayer()
+    {
+        // Poczekaj na zakoñczenie animacji przesuwania œciany.
+        yield return new WaitForSeconds(animatorEndedKillPlayer.GetCurrentAnimatorClipInfo(0)[0].clip.length);
+
+        // Zabij gracza lub zakoñcz grê.
+        Debug.Log("TheEnd");
     }
 }
