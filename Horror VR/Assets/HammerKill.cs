d440b58a83ab242067af36da6b53bfda6cf1af3e
Dehydrogenase <michalrodak98@gmail.com>
2023-09-23 20:34:16 +0200

    ceiling added + end scene added

--- Horror VR/Assets/HammerKill.cs
+++ Horror VR/Assets/HammerKill.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class HammerKill : MonoBehaviour
 {
+    
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("TheEnd");
+            SceneManager.LoadScene("End");
         }
     }
 }
